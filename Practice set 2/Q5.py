d91d35490609f4a2ea772d805308b35eb78e5964
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-04 00:58:52 +0530

    Create Q5.py

---
+++ Practice set 2/Q5.py
@@ -0,0 +1,6 @@
+# Write a python program to find the average of two numbers entered by user.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+avg = ((num1+num2)/2)
+print("Average of 2 numbers is: ", avg)

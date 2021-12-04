12d267d758a9328c9075a57f413ac5b82cbd1e24
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-05 01:09:22 +0530

    Update Q4.py

--- Practice set 3/Q4.py
+++ Practice set 3/Q4.py
@@ -1,4 +1,7 @@
 # Write a python program to detect single spaces in a string.
 
-string = "Hello, How are  you?"
-print(string.find(" "))
+# string = "Hello, How are  you?"
+# print(string.find(" "))
+
+string = input("Enter a String: ")
+print(string.find("  "))

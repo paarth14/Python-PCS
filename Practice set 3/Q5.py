ece41c895ef8b565db8a07abfd4080d8685ac9cc
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-05 01:14:33 +0530

    Create Q5.py

---
+++ Practice set 3/Q5.py
@@ -0,0 +1,6 @@
+# Write a python program to format the following variable named "letter" using escape sequence characters.
+
+letter = "Dear Harry, This Python course is nice. Thanks!"
+print(letter)
+letter1 = "Dear Harry,\nThis Python course is nice.\n\tThanks!"
+print(letter1)

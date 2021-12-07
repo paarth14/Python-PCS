4cda9cfec25eb30772e56c38ae3419038c2bb979
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-07 09:34:47 +0530

    Create Q1.py

---
+++ Practice set 4/Q1.py
@@ -0,0 +1,21 @@
+# Write a Python Program to store seven fruits in a list entered by the user.
+
+fruit1 = input("Enter first fruit: ")
+fruit2 = input("Enter second fruit: ")
+fruit3 = input("Enter third fruit: ")
+fruit4 = input("Enter fourth fruit: ")
+fruit5 = input("Enter fifth fruit: ")
+fruit6 = input("Enter sixth fruit: ")
+fruit7 = input("Enter seventh fruit: ")
+
+list1 = []
+
+list1.append(fruit1)
+list1.append(fruit2)
+list1.append(fruit3)
+list1.append(fruit4)
+list1.append(fruit5)
+list1.append(fruit6)
+list1.append(fruit7)
+
+print("\n", list1)

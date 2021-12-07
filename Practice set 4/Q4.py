a01402ad05113cebc9cdce7775cfcffb33095a1c
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-07 09:59:46 +0530

    Create Q4.py

---
+++ Practice set 4/Q4.py
@@ -0,0 +1,16 @@
+# Write a Python Program to sum a list with 4 numbers.
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+number3 = int(input("Enter third number: "))
+number4 = int(input("Enter fourth number: "))
+
+list1 = []
+
+list1.append(number1)
+list1.append(number2)
+list1.append(number3)
+list1.append(number4)
+
+
+print(list1[0] + list1[1] + list1[2] + list1[3])

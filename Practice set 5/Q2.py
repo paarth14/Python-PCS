26d4ef78dae7a3fe58d33a4404a2616b11b7cdbf
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-08 09:47:02 +0530

    Create Q2.py

---
+++ Practice set 5/Q2.py
@@ -0,0 +1,23 @@
+# Write a python program to input eight numbers from user and display all the unique numbers once.
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+num4 = int(input("Enter a number: "))
+num5 = int(input("Enter a number: "))
+num6 = int(input("Enter a number: "))
+num7 = int(input("Enter a number: "))
+num8 = int(input("Enter a number: "))
+
+set1 = set()
+
+set1.add(num1)
+set1.add(num2)
+set1.add(num3)
+set1.add(num4)
+set1.add(num5)
+set1.add(num6)
+set1.add(num7)
+set1.add(num8)
+
+print(set1)

12d367aa1343ef0997b1204e3629325cbba331b0
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-08 09:39:43 +0530

    Create Q1.py

---
+++ Practice set 5/Q1.py
@@ -0,0 +1,10 @@
+# Write a Python program to create a dictionary of Hindi words with their values as english translation. Provide user an option to look it up!
+
+mydict = {
+    "tauliya" : "towel",
+    "bistar" : "bed",
+    "chaaval" : "rice",
+    "namak" : "salt"
+}
+
+print(list(mydict.items()))

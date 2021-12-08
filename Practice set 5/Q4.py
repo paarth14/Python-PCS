b3db91d91db0a0413dfd376fd2e3741f44f31ff6
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-08 09:55:28 +0530

    Create Q4.py

---
+++ Practice set 5/Q4.py
@@ -0,0 +1,14 @@
+# What will be the length for the following set : 
+# s = set()
+# s.add(20)
+# s.add("20")
+# s.add(20.0)
+
+s = set()
+s.add(20)
+s.add("20")
+s.add(20.0)
+
+print(len(s))
+
+# OUTPUT :- 2

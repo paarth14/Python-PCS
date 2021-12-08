d43c0c452c594f392486b5012b3c1da787878c5d
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-08 09:51:28 +0530

    Create Q3.py

---
+++ Practice set 5/Q3.py
@@ -0,0 +1,10 @@
+# Can we have a set with 18(int) and "18"(str) as a values in it ?
+
+set1 = {18, "18"}
+
+print(set1)
+
+#Yes, It's Possible, Generally set only contains unique values, but in this case 18 is having two different data types.
+
+# OUTPUT :-
+# {18, '18'}

8a6d6ff3e4a5637d8b688e67755d4c9d3eb32e0a
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-09 10:10:56 +0530

    Create Q5.py

---
+++ Practice set 6/Q5.py
@@ -0,0 +1,9 @@
+# Write a python program which finds out whether the given name is present in the list or not ?
+
+names = ["Harsh", "Vansh", "Suresh", "Paarth", "Sanjay"]
+name = input("Enter your name: ")
+
+if(name in names):
+    print("Present")
+else:
+    print("Not Present")

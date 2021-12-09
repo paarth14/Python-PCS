a1ff79925a98f48f97f3872ef4e803cf3956ba63
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-12-09 10:04:47 +0530

    Create Q4.py

---
+++ Practice set 6/Q4.py
@@ -0,0 +1,8 @@
+# Write a program to find whether a given username contains less than 10 characters or not.
+
+username = input("Enter your username: ")
+
+if(len(username) <= 10):
+    print("Less than 10 characters")
+else:
+    print("More than 10 characters")
